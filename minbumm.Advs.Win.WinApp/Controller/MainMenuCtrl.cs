6fbfe5cadf744693b20f2b1f93617645ce21bbcf
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-13 20:33:01 +0900

    메뉴 컨트롤 작성

---
+++ minbumm.Advs.Win.WinApp/Controller/MainMenuCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.Win.WinApp.Controller
+{
+    public class MainMenuCtrl
+    {
+        static List<KeyValuePair<string, EventHandler>> menuEvents = new List<KeyValuePair<string, EventHandler>>();
+
+        public MainMenuCtrl() 
+        {
+            
+        }
+    }
+}
