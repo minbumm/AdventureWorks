b628abb42b7c5cc9c9a1e675f4559536385a6b90
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-17 15:48:26 +0900

    common 개발중

---
+++ minbumm.Advs.Win.Common/ConfigManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace minbumm.Advs.Win.Common
+{
+    public class ConfigManager
+    {
+        public static ConfigManager OpenConfiguration(string configPath)
+        {
+            ExeConfigurationFileMap configMap = new ExeConfigurationFileMap()
+            {
+                ExeConfigFilename = configPath
+            };
+            return ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+        }
+
+        public static ConnectionStringSettingsCollection GetConnectionStringSettings(string configFilePath)
+        {
+            ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+            configMap.ExeConfigFilename = configFilePath;
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+            if (config.ConnectionStrings != null)
+            {
+                return config.ConnectionStrings.ConnectionStrings;
+            }
+
+            return null;
+        }
+
+        public static DbCommandConfigCollection GetDbCommandCollection(string configFilePath)
+        {
+            ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+            configMap.ExeConfigFilename = configFilePath;
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+            DbCommandConfigSection dbCommandConfigSection = config.GetSection("DbCommandConfigSection") as DbCommandConfigSection;
+            if (dbCommandConfigSection != null)
+            {
+                return dbCommandConfigSection.DbCommandConfigs;
+            }
+
+            return null;
+
+        }
+
+    }    
+}
+
+
