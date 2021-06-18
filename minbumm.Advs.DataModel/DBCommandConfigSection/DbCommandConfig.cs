a313ba29c0a0f3edaeb6f0e9a78df4c5aa642a19
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-18 09:33:50 +0900

    DB 커넥션 작성

---
+++ minbumm.Advs.DataModel/DBCommandConfigSection/DbCommandConfig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.DataModel.DBCommandConfigSection
+{
+    public class DbCommandConfig : ConfigurationElement
+    {
+        public DbCommandConfig() { }
+        [ConfigurationProperty("CommandID", IsRequired = true)]
+        public string CommandID 
+        {
+            get 
+            {
+                return (string)this["CommandID"];
+            }
+            set 
+            {
+                this["CommandID"] = value;
+            }
+        }
+        
+        [ConfigurationProperty("Text", IsRequired = true)]
+        public string Text 
+        {
+            get 
+            {
+                return (string)this["Text"];
+            }
+            set 
+            {
+                this["Text"] = value;
+            }
+        }
+
+        [ConfigurationProperty("CommandType", DefaultValue = "SQL", IsRequired = true)]
+        public string CommandType
+        {
+            get
+            {
+                return (string)this["CommandType"];
+            }
+            set
+            {
+                this["CommandType"] = value;
+            }
+        }
+    }
