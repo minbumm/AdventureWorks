f95d6ef3d4761fc634dcd33c0316520ecaca1116
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-19 23:23:46 +0900

    Facade 패턴 추가

--- minbumm.Advs.Win.Common/ConfigManager.cs
+++ minbumm.Advs.Win.Common/ConfigManager.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Configuration;
 using minbumm.Advs.DataModel.DBCommandConfigSection;
 
 namespace minbumm.Advs.Win.Common
