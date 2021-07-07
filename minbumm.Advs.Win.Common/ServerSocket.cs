88d61f4cc70da3315f63f6cc95c777c9d6ab8e6b
minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-07 10:30:39 +0900

    SocketServer 작성

---
+++ minbumm.Advs.Win.Common/ServerSocket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.Win.Common
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/ko-kr/library/fx6588te(v=vs.110).aspx
+    /// </summary>
+    public class ServerSocket
+    {
+        public delegate void OnAcceptEvenHandler(ClientSocket sender);
+        public event OnAcceptEvenHandler OnAccept;
+        public delegate void OnDisconnectionEventHandler(ClientSocket sender);
+        public event OnDisconnectionEventHandler OnDisconnect;
+
+
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        private bool listeningFlag = true;
+
+        // The port number for the remote device
+    }
+}
