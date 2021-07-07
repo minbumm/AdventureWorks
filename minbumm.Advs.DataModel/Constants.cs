681e21a36722f7ad7a4f8ebc5afdccb207faf7b6
minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-07 11:20:46 +0900

    ServerSocket

---
+++ minbumm.Advs.DataModel/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.DataModel
+{
+    public class Constants
+    {
+        public const int DEFAULT_SOCKET_PORT = 10000;
+    }
+}
---
+++ minbumm.Advs.Win.Common/ServerSocketManager.cs
@@ -0,0 +1,45 @@
+﻿using minbumm.Advs.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.Win.Common
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/ko-kr/library/fx6588te(v=vs.110).aspx
+    /// </summary>
+    public class ServerSocketManager
+    {
+        public delegate void OnAcceptEvenHandler(ClientSocket sender);
+        public event OnAcceptEvenHandler OnAccept;
+        public delegate void OnDisconnectionEventHandler(ClientSocket sender);
+        public event OnDisconnectionEventHandler OnDisconnect;
+
+
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        private bool listeningFlag = true;
+
+        //The port number for the remote device
+        private int port = Constants.DEFAULT_SOCKET_PORT;
+        private string tcpServerIP = string.Empty;
+        private Encoding encoding = Encoding.Default;
+
+        //The response from the remote device
+        private string response = string.Empty;
+
+        //Client soceket define
+        Socket serverSocket = null;
+        List<ClientSocket> clients = new List<ClientSocket>();
+        Thread sockT = null;
+
+
+        public List<ClientSocket> Clients { get => clients; set => clients = value; }
+        public Socket ServerSocket { get => serverSocket; set => serverSocket = value; }
+
+
+    }
+}
