e8b52c5a07b641808f641525cc857405e8706f74
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-11 23:38:48 +0900

    암호화기능 추가

---
+++ minbumm.Advs.Win.Common/hmacsha256.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.Win.Common
+{
+    class Hmacsha256
+    {
+        public enum HMACType
+        {
+            MD5 = 0,
+            SHA1,
+            SHA2,
+            SHA256
+        }
+
+    }
+}
