f9154908dc67e52ba45d52002eadac04e4e649a6
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-15 20:37:19 +0900

    SqlBuilderHelper작성

---
+++ minbumm.Advs.Win.Common/SqlBuilderHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minbumm.Advs.Win.Common
+{
+    public static class SqlBuilderHelper
+    {
+        public static string GenerateInsert(DataRow dr, string[] removeFileds) 
+        {
+            if (dr == null)
+            {
+                throw new ArgumentNullException("dataRow");
+            }
+
+            DataTable tabel = dr.Table;
+
+            return output.ToString();
+        }
+
+    }
+}
