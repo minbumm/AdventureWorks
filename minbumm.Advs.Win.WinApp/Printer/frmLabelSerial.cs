dd0d85200964a3d553ef9cd7db27063370cbb1ec
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-19 23:04:09 +0900

    수정

---
+++ minbumm.Advs.Win.WinApp/Printer/frmLabelSerial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minbumm.Advs.Win.WinApp.Printer
+{
+    public partial class frmLabelSerial : Form
+    {
+        public frmLabelSerial()
+        {
+            InitializeComponent();
+        }
+    }
+}
